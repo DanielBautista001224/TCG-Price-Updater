23620d4cc4c475434d5f6934386cce84fdc28484
Daniel Bautista <danielbautista1224@gmail.com>
2026-06-03 00:32:44 -0500

    Se añade Revision de precio en TCG Player, no estaba disponible la opcion de API, asi que se hizo uso de un bot hecho con playwright,  tiene una efectividad del 90.61% en una prueba de 43 muestras.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 from sheets_service import get_input_data, uptade_prices
-from price_service import get_mock_price
+from price_service import TCG_Player
 
 def main():
     input_data = get_input_data()
     results = []
-
+    tcg=TCG_Player()
+    tcg.launch_tcg_player()
     for row in input_data:
-        price_usd = get_mock_price(row["NOMBRE DE LA CARTA"])
-        price_cop = round(price_usd * 4000, 2)
+        card_name = row.get("NOMBRE DE LA CARTA")
+        if not card_name or str(card_name).strip() == "":
+            
+            continue
+
+        price_usd = tcg.get_card_price(card_name+" "+row["NRO"]+" "+row["COLECCIÓN"])
+        # price_cop = round(price_usd * 4000, 2)
 
         results.append({
             "_row": row["_row"],  
@@ -15,10 +21,10 @@ def main():
             "set": row["COLECCIÓN"],
             "condition": row["NRO"],
             "price_usd": price_usd,
-            "price_cop": price_cop,
+            #"price_cop": price_cop,
             "source": "mock"
         })
-
+    tcg.Close_tcg_player()
     uptade_prices(results)
     print("Precios actualizados 🚀")
 
--- src/price_service.py
+++ src/price_service.py
@@ -1,4 +1,38 @@
-import random
+from playwright.sync_api import sync_playwright
+class TCG_Player:
+    def launch_tcg_player(self):
+        self.p = sync_playwright().start()
+        self.browser = self.p.chromium.launch(headless=False)
+        self.page = self.browser.new_page()
+        self.page.goto("https://www.tcgplayer.com/")
 
-def get_mock_price(card_name):
-    return round(random.uniform(1, 100), 2)
+
+    def get_card_price(self,card_name):
+        try: 
+            #Busqueda directa por URL
+            query = card_name.replace(" ", "+")
+            url = f"https://www.tcgplayer.com/search/all/product?q={query}&view=grid"
+            self.page.goto(url)
+            #espera a que cargue la pagina
+            self.page.wait_for_selector(".search-result",timeout=10000)
+
+            #revision del precio del primer resultado
+            self.page.wait_for_selector("a[data-testid^='product-card__image']",timeout=10000)
+            first_product = self.page.locator("a[data-testid^='product-card__image']").first
+            price = first_product.locator(".product-card__market-price--value").inner_text()
+            result_name = first_product.locator(".product-card__title").inner_text().strip()
+            print(card_name)
+            print(result_name)
+            print(price)
+            price_float=float(price.replace("$", "").replace(",", ""))
+
+            return price_float
+        except Exception as e:
+            print(f"❌ Error con '{card_name}': {e}")
+            return ("no encontrado")
+
+
+
+    def Close_tcg_player(self):
+        self.browser.close()
+        self.p.stop()
